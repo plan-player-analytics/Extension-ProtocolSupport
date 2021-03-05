c2931c38a3f1fbfdc4ac4421453c72159a85e74d
Risto Lahtela <24460436+AuroraLS3@users.noreply.github.com>
2021-03-05 13:49:33 +0200

    Changed repository

--- src/main/java/com/djrapitops/extension/ProtocolSupportExtension.java
+++ src/main/java/com/djrapitops/extension/ProtocolSupportExtension.java
@@ -1,5 +1,5 @@
 /*
-    Copyright(c) 2019 Risto Lahtela (Rsl1122)
+    Copyright(c) 2019 Risto Lahtela (AuroraLS3)
 
     The MIT License(MIT)
 
@@ -38,7 +38,7 @@ import java.util.UUID;
 /**
  * ProtocolSupport DataExtension.
  *
- * @author Rsl1122
+ * @author AuroraLS3
  */
 @PluginInfo(name = "ProtocolSupport", iconName = "gamepad", iconFamily = Family.SOLID, color = Color.CYAN)
 public class ProtocolSupportExtension implements DataExtension {
--- src/main/java/com/djrapitops/extension/ProtocolSupportVersionListener.java
+++ src/main/java/com/djrapitops/extension/ProtocolSupportVersionListener.java
@@ -1,5 +1,5 @@
 /*
-    Copyright(c) 2019 Risto Lahtela (Rsl1122)
+    Copyright(c) 2019 Risto Lahtela (AuroraLS3)
 
     The MIT License(MIT)
 
